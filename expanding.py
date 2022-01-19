3fd28b99bdad35401ba4717bcc5d0905d334d587
Amir khan <amirkhan1092@gmail.com>
2022-01-19 11:45:35 +0530

    try again

--- expanding.py
+++ expanding.py
@@ -1,13 +1,6 @@
 def expanding(a):
-    c=len(a)
-    lst=[]
-    for i in range(c-1):
-        d=a[i]-a[i+1]
-        lst.append(abs(d))
-    if lst==sorted(lst):
-        return True
-    else:
-        return False
+    # try again 
+    pass
 a=eval(input())
 out=expanding(a)
 print(out)
